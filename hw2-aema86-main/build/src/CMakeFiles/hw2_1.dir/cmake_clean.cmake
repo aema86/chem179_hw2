9fe3583cda95af9817dac280adc6ad3796b45cc0
aema86 <aema86@berkeley.edu>
2025-02-25 16:19:11 -0800

    Add files via upload

---
+++ hw2-aema86-main/build/src/CMakeFiles/hw2_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2_1.dir/hw2_1.cpp.o"
+  "CMakeFiles/hw2_1.dir/hw2_1.cpp.o.d"
+  "hw2_1"
+  "hw2_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
